b4f3724dd08b33d4618d43cf63821e5baf37296d
janmanchi <jakshaya930@gmail.com>
2025-08-02 12:30:41 +0530

    Fourth update

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ def index():
     return render_template("index.html")
 @app.route("/home")
 def home():
-    return "Welcome to Homepage"
+    return render_template("home.html")
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
